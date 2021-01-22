b1eb5691f9dac0d579ee849f72f54c6140e1360e
tongwai <anthony@voicetube.com>
2021-01-22 12:49:28 +0800

    [feat] startup server

---
+++ run_server.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+set -e
+
+if [[ ! -f /var/log/gunicorn ]]; then
+    mkdir -p /var/log/gunicorn
+fi
+
+gunicorn_config='gunicorn_config.py'
+if [ "$1" == "dev" ]; then
+    gunicorn_config='gunicorn_config_dev.py'
+fi
+
+gunicorn \
+    --config "/app/src/${gunicorn_config}" \
+    search_app:app
+
+exec "$@"
